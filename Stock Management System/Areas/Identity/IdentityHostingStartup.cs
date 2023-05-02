7412248bfb206bd807c45d76ae231ae0fa417187
Salina Budhathoki <68579567+immsaleena@users.noreply.github.com>
2023-05-02 22:20:35 +0900

    Add files via upload

---
+++ Stock Management System/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using coursework.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(coursework.Areas.Identity.IdentityHostingStartup))]
+namespace coursework.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
